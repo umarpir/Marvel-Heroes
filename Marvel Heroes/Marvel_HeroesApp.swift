066e4512f0356dfdae5d6172ebfe0bbf3128f63d
Umar Pirmahomed <umarpirmahomed786@gmail.com>
2023-03-19 19:42:32 +0000

    Initial Commit

---
+++ Marvel Heroes/Marvel_HeroesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Marvel_HeroesApp.swift
+//  Marvel Heroes
+//
+//  Created by Umar Pirmahomed on 19/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Marvel_HeroesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
